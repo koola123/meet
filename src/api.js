e0a3e5d4d5caaa84e3f599f81bdee3d286411b4a
koola123 <82808273+koola123@users.noreply.github.com>
2021-10-04 14:14:41 +0200

    updated api.js

--- src/api.js
+++ src/api.js
@@ -38,13 +38,11 @@ export const getEvents = async () => {
 
 
   const token = await getAccessToken();
-
   if (token) {
     removeQuery();
     const url = 'https://sdzyveutib.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url' + '/' + token;
     const result = await axios.get(url);
     if (result.data) {
-      console.log(result)
       var locations = extractLocations(result.data.events);
       localStorage.setItem("lastEvents", JSON.stringify(result.data));
       localStorage.setItem("locations", JSON.stringify(locations));
