4e0da268c2e1f08a4e96641a948d1972f153cd33
Matthias Müringer <mmueringer@web.de>
2023-04-22 20:54:11 +0200

    feat: installed Atatus

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
+import * as atatus from 'atatus-spa';
+atatus.config('5ebafcb8711b41709596d6da6c01d1c2').install();
 
 ReactDOM.render(
   <React.StrictMode>
@@ -20,3 +22,5 @@ serviceWorkerRegistration.unregister();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 reportWebVitals();
+
+
