9d59b4eb24b911466df1f2144dc493f70d008bba
Matthias Müringer <mmueringer@web.de>
2023-04-27 18:35:49 +0200

    fix: refactored tests and welcome msg

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -1,33 +1,40 @@
-import React from 'react';
+import React from "react";
 
 function WelcomeScreen(props) {
-    return props.showWelcomeScreen ? (
-        <div className="WelcomeScreen">   
-            <h1>Welcome to the Meet app</h1>
-            <h4>
-                Log in to see upcoming events around the world for 
-                full-stack 
-                developers
-            </h4>
-            <div className="button_cont" align="center">
-                <div classnName="google-btn">
-                    <div className="google-icon-wrapper">
-                        <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Log
-                        o.svg" alt="Google sign-in" />
-                    </div>
-                    <button onClick={() => {props.getAccessToken()}}
-                    rel="nofollow noopener"
-                    className="btn-text">
-                     <b>Sign in with Google</b>       
-                    </button>
-                </div>
-            </div>
-            <a href="https://koola123.github.io/meet/privacy.html" rel="nofollow noopener">
-                Privacy Policy
-            </a>
+  return props.showWelcomeScreen ? (
+    <div className="WelcomeScreen">
+      <h1>Welcome to the Meet app</h1>
+      <h4>
+        Log in to see upcoming events around the world for full-stack developers
+      </h4>
+      <div className="button_cont" align="center">
+        <div classnName="google-btn">
+          <div className="google-icon-wrapper">
+            <img
+              className="google-icon"
+              src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+              alt="Google sign-in"
+            />
+          </div>
+          <button
+            onClick={() => {
+              props.getAccessToken();
+            }}
+            rel="nofollow noopener"
+            className="btn-text"
+          >
+            <b>Sign in with Google</b>
+          </button>
         </div>
-    ) : null
+      </div>
+      <a
+        href="https://koola123.github.io/meet/privacy.html"
+        rel="nofollow noopener"
+      >
+        Privacy Policy
+      </a>
+    </div>
+  ) : null;
 }
-
 
 export default WelcomeScreen;
