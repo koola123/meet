e90fa36f49a6d64916e5e63dd2c97682dcff13e8
Matthias Müringer <mmueringer@web.de>
2023-04-27 18:38:49 +0200

    fix: WelcomeScreen

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -8,7 +8,7 @@ function WelcomeScreen(props) {
         Log in to see upcoming events around the world for full-stack developers
       </h4>
       <div className="button_cont" align="center">
-        <div classnName="google-btn">
+        <div className="google-btn">
           <div className="google-icon-wrapper">
             <img
               className="google-icon"
